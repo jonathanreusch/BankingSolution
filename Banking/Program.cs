95fbec0abe4490b4280580300312ee307d5041c7
Jonathan Reusch <jonathanreusch1@gmail.com>
2022-01-19 21:38:37 -0500

    Add project files.

---
+++ Banking/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Banking
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            var acct1 = new Account(1);
+            acct1.Deposit(100);
+
+        }
+
+
+    }
+}
